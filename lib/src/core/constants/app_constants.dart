0c5d07a22954f89402c968aa42c1a1e62b9e48b1
Ahmet Muhammet Subaşı <ahmet.subasi@kodzillaistanbul.com>
2023-06-17 22:43:53 +0300

    created app constants

---
+++ lib/src/core/constants/app_constants.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names, unnecessary_raw_strings
+
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ApplicationConstants {
+  // static final sentryDSN = ;
+  static const leftIcon = 'assets/icon/icon-left.svg';
+}
