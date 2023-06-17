c9a1055b2bc7b924e636f23fd9eb5a22988e38cd
Ahmet Muhammet Subaşı <ahmet.subasi@kodzillaistanbul.com>
2023-06-17 22:43:40 +0300

    new color

---
+++ lib/src/core/theme/color_tones.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorTones {
+  static Color primaryBlue = const Color(0xFF377DFF);
+  static Color lightBlue = const Color(0xffE5F1FF);
+  static Color inactiveRed = const Color(0xffca2d61);
+  static Color activeGreen = const Color(0xff2DCA8C);
+  static Color viewBackground = const Color(0xffF7F7F9);
+  static Color grayText = const Color(0xffAAB0B7);
+  static Color primaryText = const Color(0xff243443);
+}
